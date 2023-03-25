5b755b33b079eb91fddf22b289405b714a275c6f
ZongyangYue <yuezongyang210@gmail.com>
2023-03-24 18:53:44 -0700

    add data members to TcpServer class

--- http_tcpServer_linux.h
+++ http_tcpServer_linux.h
@@ -10,12 +10,20 @@ namespace http
     class TcpServer
     {
     public:
-        TcpServer();
+        TcpServer(std::string ip_address, int port);
         ~TcpServer();
 
     private:
+        std::string m_ip_address;
+        int m_port;
+
         int m_socket;
         int m_new_socket;
+
+        long m_incomingMessage;
+        struct sockaddr_in m_socketAddress;
+        unsigned int m_socketAddress_len;
+        std::string m_serverMessage;
 
         int startServer();
         void closeServer();
